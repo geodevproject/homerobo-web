86f92a8044c8386bf3fe5f132b12d70a6b71fee5
Jason Barnett <Jason@Jasons-MacBook-Pro.local>
2020-11-19 21:31:55 -0500

    testing redis

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,19 @@
  $(document).ready(function() {
 
     $("#start").click(function(event){
-       $.getJSON('http://127.0.0.1:5000/api/homerobo/start', function(jd) {
+       $.getJSON('http://127.0.0.1:8080/api/homerobo/start', function(jd) {
           $('#stage').html('<p> Message: ' + jd.message + '</p>');
        });
     });
 
     $("#medium").click(function(event){
-       $.getJSON('http://127.0.0.1:5000/api/homerobo/medium', function(jd) {
+       $.getJSON('http://127.0.0.1:8080/api/homerobo/medium', function(jd) {
           $('#stage').html('<p> Message: ' + jd.message + '</p>');
        });
     });
 
     $("#stop").click(function(event){
-       $.getJSON('http://127.0.0.1:5000/api/homerobo/stop', function(jd) {
+       $.getJSON('http://127.0.0.1:8080/api/homerobo/stop', function(jd) {
           $('#stage').html('<p> Message: ' + jd.message + '</p>');
        });
     });
